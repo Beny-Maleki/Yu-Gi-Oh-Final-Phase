95fbec80d213a20058a16ea544baba4391085d17
Beny-Maleki <benyaminmaleki81@gmail.com>
2021-07-20 11:56:08 +0430

    bug fix in Listener

--- src/main/java/client/network/ClientListener.java
+++ src/main/java/client/network/ClientListener.java
@@ -73,15 +73,6 @@ public class ClientListener extends Thread {
                 case PROFILE:
             }
 
-            Controller.setResponseCommand(serverResponse);
-            Controller.setResponseException(serverResponse.getException());
-
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            serverResponse.setCommandType(CommandType.WAITING);
         }
     }
 }
